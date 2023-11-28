184841aca66724448cd372b89099f1436510afde
python-benedict <newlyf38@gmail.com>
2023-11-28 09:18:25 +0000

    added an edit_document function to the views.py to edit the created document

--- text_editor_app/views.py
+++ text_editor_app/views.py
@@ -6,3 +6,6 @@ def index(request):
     documents = Document.objects.all()
     return render(request, 'text_editor/index.html', {'documents': documents})
 
+def edit_document(request, document_id):
+    document = get_object_or_404(Document, pk=document_id)
+    return render(request, 'text_editor/edit_document.html', {'document': document})
