e0fcffe89e0199f006bbc32491de1177b12331e7
python-benedict <newlyf38@gmail.com>
2023-11-28 09:17:25 +0000

    added an index function to the views.py project to get all the created files

--- text_editor_app/views.py
+++ text_editor_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Document
 # Create your views here.
+
+def index(request):
+    documents = Document.objects.all()
+    return render(request, 'text_editor/index.html', {'documents': documents})
+
